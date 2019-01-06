ef51de970b4c8c542c092c387a1bfda3564714af
DBA presents <ten-arek@o2.pl>
2019-01-06 21:06:57 +0100

    HtmlWebpackPlugin installed and configured to generate HTML files.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: {
         races: "./main/webapp/scripts/races.js",
@@ -6,5 +8,19 @@ module.exports = {
     output: {
         path: __dirname + "/main/webapp/dist/",
         filename: "scripts/[name].bundle.js"
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./main/webapp/races.html",
+            filename: "races.html",
+            inject: 'head',
+            chunks: ['races']
+        }),
+        new HtmlWebpackPlugin({
+            template: "./main/webapp/results.html",
+            filename: "results.html",
+            inject: 'head',
+            chunks: ['results']
+        })
+    ]
 };
