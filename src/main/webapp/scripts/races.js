68c85e0928b7bd22c4faf3beb7a236eb269c1095
DBA presents <ten-arek@o2.pl>
2019-07-01 07:30:47 +0200

    jQuery UI dialog used

--- src/main/webapp/scripts/races.js
+++ src/main/webapp/scripts/races.js
@@ -2,16 +2,27 @@
 
 import '../styles/main.scss';
 import $ from 'jquery';
+import 'jquery-ui/ui/widgets/dialog';
 
 (function($) {
     function addRace(year) {
         const hlink = "results.html?year=" + year;
-        $("#races").append("<li><a href='" + hlink + "'>" + year + "</a></li>")
+        $("#races").append("<li><a href='" + hlink + "'>" + year + "</a> <button type = \"button\" class = \"btn btn-info race-info-btn\">Info</button></li>");
     }
 
     window.onload = (function () {
         addRace("2018");
         addRace("2017");
+
+        $(".race-info-btn").click(function() {
+            $("#raceInfoDiv").dialog({
+                resizable: false,
+                height: "auto",
+                width: 500,
+                modal: true,
+                buttons: []
+            });
+        });
     });
 
 })($);
