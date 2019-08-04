d6985589fb171ad3eb7920fbce8042c56c358238
DBA presents <ten-arek@o2.pl>
2019-08-04 17:04:30 +0200

    ES6 lambda function used.

--- src/main/webapp/scripts/urlparams.js
+++ src/main/webapp/scripts/urlparams.js
@@ -1,19 +1,11 @@
 "use strict";
 
-var urlparams = (function() {
+function getUrlParam(param) {
+    const url = new URL(window.location.href);
+    return url.searchParams.get(param);
+}
 
-    function getUrlParam(param) {
-        const url = new URL(window.location.href);
-        return url.searchParams.get(param);
-    }
-
-    return {
-        getUrlParam: getUrlParam
-    }
-
-})();
-
-module.exports = {
-    getUrlParam: urlparams.getUrlParam
-};
+export {
+    getUrlParam
+}
 
